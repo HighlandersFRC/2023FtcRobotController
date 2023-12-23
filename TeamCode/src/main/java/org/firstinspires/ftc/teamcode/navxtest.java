504dec66c06b21b1d66b1bc16df5a529d3be6573
ethanlululu <90187@psdschools.org>
2023-12-23 12:01:50 -0700

    navX ahrs works

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/navxtest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/navxtest.java
@@ -1,6 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 import com.kauailabs.navx.ftc.AHRS;
-public class navxtest  {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public abstract class navxtest extends LinearOpMode {
     private AHRS navX;
 
 
