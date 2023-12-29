5910582aa577f8a4d90ac440072ae4235e37bb59
ethanlululu <90187@psdschools.org>
2023-12-29 14:24:31 -0700

    turn command navx and drive command work

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autos/navxcmdtest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autos/navxcmdtest.java
@@ -28,10 +28,11 @@ public class navxcmdtest extends LinearOpMode {
     public void runOpMode() {
         waitForStart();
 
+
         scheduler.add(new CommandGroup(scheduler,
                /* new ParallelCommandGroup(scheduler, new Drive(hardwareMap, 0.2, 1), new CommandGroup(scheduler, new Wait(1000), new DeployIntake(hardwareMap, "Deploy"))),
                 new MoveWrist(hardwareMap, Constants.wristDown),*/
-                new turn2(hardwareMap, 90)
+                new turn2(hardwareMap, 45)
               /*  new drive2(hardwareMap, -0.2, 0.05),
                 new ParallelCommandGroup(scheduler, new Drive(hardwareMap, 0.3, -0.1), new PixelTray(hardwareMap, 3000, -1, "R"), new CommandGroup(scheduler, new Wait(1000), new Intake(hardwareMap, 1000, -0.25))),
                 new MoveWrist(hardwareMap, Constants.wristUp),
