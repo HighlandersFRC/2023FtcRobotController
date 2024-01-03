115609f2215f028bcfe424ced349d6bc77425eeb
ethanlululu <90187@psdschools.org>
2024-01-03 11:35:00 -0700

    wrist subsystem

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Wrist.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Wrist.java
@@ -1,4 +1,11 @@
 package org.firstinspires.ftc.teamcode.Subsystems;
-
-public class Wrist {
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+public class Wrist extends Subsystems {
+    String name = "WristServo";
+    public static Servo WristServo;
+    public static void Wrist(HardwareMap hardwareMap,double TargetPos){
+        WristServo = hardwareMap.servo.get("WristServo");
+        WristServo.setPosition(TargetPos);
+    }
 }
