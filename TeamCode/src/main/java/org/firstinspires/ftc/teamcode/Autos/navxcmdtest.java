5a6671102fef639175c041448101f32be85c21a8
ethanlululu <90187@psdschools.org>
2024-01-05 15:23:03 -0700

    strafe should work

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autos/navxcmdtest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autos/navxcmdtest.java
@@ -3,6 +3,10 @@ package org.firstinspires.ftc.teamcode.Autos;
 import com.kauailabs.navx.ftc.AHRS;
 import com.qualcomm.hardware.kauailabs.NavxMicroNavigationSensor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.Commands.strafeLeft;
+import org.firstinspires.ftc.teamcode.Commands.strafeLeft;
+import org.firstinspires.ftc.teamcode.Commands.strafeRight;
 import org.firstinspires.ftc.teamcode.Commands.turn2;
 import org.firstinspires.ftc.teamcode.Commands.CommandGroup;
 import org.firstinspires.ftc.teamcode.Commands.Scheduler;
@@ -21,8 +25,9 @@ public class navxcmdtest extends LinearOpMode {
         scheduler.add(new CommandGroup(scheduler,
                /* new ParallelCommandGroup(scheduler, new Drive(hardwareMap, 0.2, 1), new CommandGroup(scheduler, new Wait(1000), new DeployIntake(hardwareMap, "Deploy"))),
                 new MoveWrist(hardwareMap, Constants.wristDown),*/
-                new turn3(hardwareMap, 180)
-
+                //new turn3(hardwareMap, 270)
+                //new strafeLeft(hardwareMap,.2,100000)
+                new strafeRight(hardwareMap,.2,100000)
               /*  new drive2(hardwareMap, -0.2, 0.05),
                 new ParallelCommandGroup(scheduler, new Drive(hardwareMap, 0.3, -0.1), new PixelTray(hardwareMap, 3000, -1, "R"), new CommandGroup(scheduler, new Wait(1000), new Intake(hardwareMap, 1000, -0.25))),
                 new MoveWrist(hardwareMap, Constants.wristUp),
