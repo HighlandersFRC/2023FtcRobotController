c89903617ef4d948346a6dbae43778054d8ff048
Dhairya Trivedi <140292400+Dhairya-exe@users.noreply.github.com>
2024-01-15 16:17:35 -0700

    added imu in the peripherals subsystem

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Peripherals.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Peripherals.java
@@ -2,14 +2,31 @@ package org.firstinspires.ftc.teamcode.Subsystems;
 
 import com.kauailabs.navx.ftc.AHRS;
 import com.qualcomm.hardware.kauailabs.NavxMicroNavigationSensor;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 public class Peripherals extends Subsystems{
     public static String name = "Peripherals";
     public static AHRS navX;
+    public static IMU imu;
+
 
     public static void initialize(HardwareMap hardwareMap){
+        imu = hardwareMap.get(IMU.class, "imu");
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.RIGHT,
+                RevHubOrientationOnRobot.UsbFacingDirection.UP));
+        imu.initialize(parameters);
         navX = com.kauailabs.navx.ftc.AHRS.getInstance(hardwareMap.get(NavxMicroNavigationSensor.class, "navX"), AHRS.DeviceDataType.kProcessedData);
+    }
+    public static double getimuYaw(){
+        return imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES);
+    }
+    public static void zeroIMU(){
+        imu.resetYaw();
     }
     public static double getYaw(){
         return navX.getYaw();
