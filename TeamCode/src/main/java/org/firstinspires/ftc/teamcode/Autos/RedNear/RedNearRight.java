cf58cffa1831c85a48c2eef67a470ea2351fdd43
ethanlululu <90187@psdschools.org>
2024-01-15 16:19:48 -0700

    worked on autos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autos/RedNear/RedNearRight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autos/RedNear/RedNearRight.java
@@ -1,47 +1,36 @@
 package org.firstinspires.ftc.teamcode.Autos.RedNear;
-
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-
-import org.firstinspires.ftc.teamcode.Tools.Constants;
+import org.firstinspires.ftc.teamcode.Commands.Arm;
 import org.firstinspires.ftc.teamcode.Commands.CommandGroup;
-import org.firstinspires.ftc.teamcode.Commands.OldCommands.DeployIntake;
 import org.firstinspires.ftc.teamcode.Commands.Drive;
 import org.firstinspires.ftc.teamcode.Commands.MainIntake;
 import org.firstinspires.ftc.teamcode.Commands.MoveWrist;
 import org.firstinspires.ftc.teamcode.Commands.ParallelCommandGroup;
-import org.firstinspires.ftc.teamcode.Commands.PixelTray;
-import org.firstinspires.ftc.teamcode.Commands.OldCommands.RetractIntake;
-import org.firstinspires.ftc.teamcode.Commands.OldCommands.RotateArm;
 import org.firstinspires.ftc.teamcode.Commands.Scheduler;
 import org.firstinspires.ftc.teamcode.Commands.Turn;
-import org.firstinspires.ftc.teamcode.Commands.Wait;
+import org.firstinspires.ftc.teamcode.Tools.Constants;
 
 @Autonomous
-@Disabled
 public class RedNearRight extends LinearOpMode {
-    @Override
+
+    Scheduler scheduler = new Scheduler();
     public void runOpMode() throws InterruptedException {
-        Scheduler scheduler = new Scheduler();
         waitForStart();
         scheduler.add(new CommandGroup(scheduler,
-                new ParallelCommandGroup(scheduler, new Drive(hardwareMap, 0.2, 0.675), new CommandGroup(scheduler, new Wait(1000), new DeployIntake(hardwareMap, "Deploy"))),
-                new MoveWrist(hardwareMap, Constants.wristDown),
+                new MoveWrist(hardwareMap,Constants.wristDown),
+                new Drive(hardwareMap, -0.3, -0.13),
                 new Turn(hardwareMap, -90),
-                new Drive(hardwareMap, 0.3, 0.45),
-                new ParallelCommandGroup(scheduler, new Drive(hardwareMap, 0.15, -0.1), new PixelTray(hardwareMap, 3000, -1, "L"), new CommandGroup(scheduler, new Wait(1000),  new MainIntake(hardwareMap, 1000, -0.25))),
-                new MoveWrist(hardwareMap, Constants.wristUp),
-                new Wait(1000),
-                new RetractIntake(hardwareMap),
-                new Drive(hardwareMap, 0.25, 0.34),
-                new DeployIntake(hardwareMap, "Deploy"),
-                new Wait(1000),
-                new RotateArm(hardwareMap, Constants.armPlace),
-                new ParallelCommandGroup(scheduler, new PixelTray(hardwareMap, 3000, -1, "R"), new RotateArm(hardwareMap, Constants.armPlace))
+                new MoveWrist(hardwareMap, Constants.wristDown),
+                new MainIntake(hardwareMap, 1000, -0.2),
+                new Turn(hardwareMap, -180),
+                new ParallelCommandGroup(scheduler, new Drive(hardwareMap,-0.5,0.71), new Arm(hardwareMap, Constants.armPlace)),
+                new MoveWrist(hardwareMap,Constants.wristUp),
+                new MainIntake(hardwareMap, 3000, -0.5)
         ));
-        while (opModeIsActive()){
+        while(opModeIsActive()){
             scheduler.update();
         }
     }
 }
+///
