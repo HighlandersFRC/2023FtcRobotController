50e087ba08375c25cd4b79d0be4d401d6a72beba
Ke Qu <143046750+Kequ090701@users.noreply.github.com>
2024-01-15 16:45:48 -0700

    NewAutos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autos/RedNear/RedNearMiddle.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autos/RedNear/RedNearMiddle.java
@@ -32,7 +32,7 @@ public class RedNearMiddle extends LinearOpMode {
                 new ParallelCommandGroup(scheduler, new Drive(hardwareMap, 0.2, 0.37), new CommandGroup(scheduler, new Wait(1000), new DeployIntake(hardwareMap, "Deploy"))),
                 new MoveWrist(hardwareMap, Constants.wristDown),
                 new Turn(hardwareMap, 179),
-                new Drive(hardwareMap, -0.2, 0.4),
+                new Drive(hardwareMap, 0.2, 0.4),
                 new ParallelCommandGroup(scheduler, new Drive(hardwareMap, 0.15, -0.1), new PixelTray(hardwareMap, 3000, -1, "L"), new CommandGroup(scheduler, new Wait(1000), new MainIntake(hardwareMap, 1000, -0.25))),
                 new MoveWrist(hardwareMap, Constants.wristUp),
                 new Wait(1000),
