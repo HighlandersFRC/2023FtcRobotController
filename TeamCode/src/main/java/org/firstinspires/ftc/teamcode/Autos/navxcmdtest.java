c1b7ccb3fefe35d2cdaa0824efac1c075a6ab44d
Dhairya Trivedi <140292400+Dhairya-exe@users.noreply.github.com>
2024-01-20 17:10:34 -0700

    All red side autos work

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autos/navxcmdtest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autos/navxcmdtest.java
@@ -2,7 +2,10 @@ package org.firstinspires.ftc.teamcode.Autos;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.teamcode.Commands.navxzero;
+import org.firstinspires.ftc.teamcode.Commands.CommandGroup;
 import org.firstinspires.ftc.teamcode.Commands.Scheduler;
+import org.firstinspires.ftc.teamcode.Commands.strafe;
 
 @com.qualcomm.robotcore.eventloop.opmode.Autonomous
 
@@ -13,6 +16,21 @@ public class navxcmdtest extends LinearOpMode {
     public void runOpMode() {
         waitForStart();
 
+        scheduler.add(new CommandGroup(scheduler,
+               /* new ParallelCommandGroup(scheduler, new Drive(hardwareMap, 0.2, 1), new CommandGroup(scheduler, new Wait(1000), new DeployIntake(hardwareMap, "Deploy"))),
+                new MoveWrist(hardwareMap, Constants.wristDown),*/
+                new strafe(hardwareMap, 1, 1)
+
+              /*  new drive2(hardwareMap, -0.2, 0.05),
+                new ParallelCommandGroup(scheduler, new Drive(hardwareMap, 0.3, -0.1), new PixelTray(hardwareMap, 3000, -1, "R"), new CommandGroup(scheduler, new Wait(1000), new Intake(hardwareMap, 1000, -0.25))),
+                new MoveWrist(hardwareMap, Constants.wristUp),
+                new Wait(1000),
+                new DeployIntake(hardwareMap, "Retract"),
+                new drive2(hardwareMap, 0.3, 0.1),
+                new DeployIntake(hardwareMap, "Deploy"),
+                new Wait(1500),
+                new MoveWrist(hardwareMap, Constants.wristDown)*/
+        ));
         while (opModeIsActive())
         {
             scheduler.update();
