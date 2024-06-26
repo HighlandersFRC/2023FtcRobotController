fbe93dfe7962863b15670e8ca5c8baec343ef2c0
Advik-highlanders28 <144296737+Advik-highlanders28@users.noreply.github.com>
2024-06-25 22:46:44 -0600

    April Tag Detection Code revised

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTagDetection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTagDetection.java
@@ -8,6 +8,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.ExposureControl;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.GainControl;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagGameDatabase;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
@@ -61,7 +62,7 @@ exposure.setExposure(15, TimeUnit.MILLISECONDS);
 
             if (tagProcessor.getDetections() .size() > 0) {
                 org.firstinspires.ftc.vision.apriltag.AprilTagDetection tag = tagProcessor.getDetections().get(0);
-
+                DistanceUnit Unit = DistanceUnit.CM;
 
 
 //telemetry for testing once tested range will help get distances for localization
