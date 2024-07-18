1066976609cb6fc5308b59936bf09acf2b5b4465
Ethan Lu <90187@passchools.org>
2024-07-18 13:49:08 -0600

    Getting values from odo pod

--- TeamCode/src/main/java/Test.java
+++ TeamCode/src/main/java/Test.java
@@ -1,11 +1,22 @@
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.Subsystems.DriveSubsystem;
+import org.firstinspires.ftc.teamcode.Subsystems.Odometry;
 
 @Autonomous
 public class Test extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
+        Odometry.initialize(hardwareMap);
+        waitForStart();
+        while (opModeIsActive()){
+        Odometry.getPos();
+        System.out.println("R_R"+Odometry.getRotationR());
+        System.out.println("R_L"+Odometry.getRotationL());
+        System.out.println("meterR"+Odometry.getMeterR());
+        System.out.println("meterL"+Odometry.getMeterL());
+        }
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAuto.java
@@ -1,32 +1,32 @@
-package org.firstinspires.ftc.teamcode;
-
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-
-import org.firstinspires.ftc.teamcode.Commands.CommandScheduler;
-import org.firstinspires.ftc.teamcode.Commands.ParallelCommandGroup;
-import org.firstinspires.ftc.teamcode.Commands.SequentialCommandGroup;
-import org.firstinspires.ftc.teamcode.Commands.TestCommand;
-import org.firstinspires.ftc.teamcode.Commands.Wait;
-import org.firstinspires.ftc.teamcode.Tools.Parameters;
-
-@Autonomous(name="TestAuto", group="Linear Opmode")
-public class TestAuto extends LinearOpMode {
-
-    @Override
-    public void runOpMode() throws InterruptedException {
-        CommandScheduler scheduler = CommandScheduler.getInstance();
-        scheduler.schedule(
-                new SequentialCommandGroup(
-                        new ParallelCommandGroup(Parameters.ANY, new Wait(100000000), new Wait(10000)),
-                        new TestCommand()
-                )
-        );
-
-        waitForStart();
-
-        while (opModeIsActive()) {
-            scheduler.run();
-        }
-    }
-}
+//package org.firstinspires.ftc.teamcode;
+//
+//import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+//import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+//
+//import org.firstinspires.ftc.teamcode.Commands.CommandScheduler;
+//import org.firstinspires.ftc.teamcode.Commands.ParallelCommandGroup;
+//import org.firstinspires.ftc.teamcode.Commands.SequentialCommandGroup;
+//import org.firstinspires.ftc.teamcode.Commands.TestCommand;
+//import org.firstinspires.ftc.teamcode.Commands.Wait;
+//import org.firstinspires.ftc.teamcode.Tools.Parameters;
+//
+//@Autonomous(name="TestAuto", group="Linear Opmode")
+//public class TestAuto extends LinearOpMode {
+//
+//    @Override
+//    public void runOpMode() throws InterruptedException {
+//        CommandScheduler scheduler = CommandScheduler.getInstance();
+//        scheduler.schedule(
+//                new SequentialCommandGroup(
+//                        new ParallelCommandGroup(Parameters.ANY, new Wait(100000000), new Wait(10000)),
+//                        new TestCommand()
+//                )
+//        );
+//
+//        waitForStart();
+//
+//        while (opModeIsActive()) {
+//            scheduler.run();
+//        }
+//    }
+//}
