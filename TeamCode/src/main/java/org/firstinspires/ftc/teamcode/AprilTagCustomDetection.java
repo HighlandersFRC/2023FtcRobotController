038225985d5481f645675e3c64f6b96a24754623
Advik-highlanders28 <144296737+Advik-highlanders28@users.noreply.github.com>
2024-07-27 12:48:41 -0600

    Localization code fully modular with hashmap

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTagCustomDetection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTagCustomDetection.java
@@ -80,17 +80,25 @@ public class AprilTagCustomDetection extends LinearOpMode {
 
                     AprilTagPoseFtc pose = new AprilTagPoseFtc(x, y, z, yaw, roll, pitch, range, bearing, elevation);
 //corrected range uses a quartic regression to calculate an offset found in Constants
-                  */
+                  *//*
+
+*/
 /*  double distance = Constants.fiveCorrected(pose.range);*//*
+*/
+/*
 
                     double CorrectX = Constants.yCorrected(pose.y);
                     double CorrectY =  -Constants.xCorrected(pose.x);
 
                     double measuredz = 5;
 
-               */
+               *//*
+
+*/
 /*     double YC = Math.cos(pose.bearing) * distance;
                     double XC = Math.sin(pose.bearing) * distance;*//*
+*/
+/*
 
 
                     //Polar coordinates
@@ -108,12 +116,18 @@ public class AprilTagCustomDetection extends LinearOpMode {
                     double FieldX = xt+0;//x value of library of vector f note look at tag to see which value
                     double FieldY= yt+0;//y value of library of vector f look at tag to see coordinates
 
-                  */
+                  *//*
+
+*/
 /*double YC = Math.cos(Cbearing)*accuratedistance;
                   double XC = Math.sin(Cbearing)*accuratedistance;
 
                   double xf = 0;
                   double yf = 0;*//*
+*/
+/*
+
+*//*
 
 */
 /*
@@ -122,14 +136,24 @@ public class AprilTagCustomDetection extends LinearOpMode {
 
                   double Xf = Math.sqrt(CorrectX*CorrectX+CorrectY*CorrectY)*(Math.cos(90-(Math.atan2(CorrectX,CorrectY)+pose.yaw)));
                   double Yf =Math.sqrt(CorrectX*CorrectX+CorrectY*CorrectY)*(Math.sin(90-(Math.atan2(CorrectX,CorrectY)+pose.yaw)));*//*
+*/
+/*
 
 
-                */
+                *//*
+
+*/
 /*    telemetry.addData("accuratedistance", accuratedistance);
                     telemetry.addData("bearing", Cbearing);*//*
+*/
+/*
 
-                 */
+                 *//*
+
+*/
 /*   telemetry.addData("not corrected distance",pose.range);*//*
+*/
+/*
 
                     telemetry.addData("pose", String.format("(%.2f, %.2f)",FieldX,FieldY));
                     telemetry.addData("CorrectX", CorrectX);
@@ -148,16 +172,24 @@ public class AprilTagCustomDetection extends LinearOpMode {
                     telemetry.addData("yaw", pose.yaw);
                     telemetry.addData("bearing (horizontal angle)", pose.bearing);
                     telemetry.addData("elevation (vertical angle)", pose.elevation);
-           */
+           *//*
+
+*/
 /*         telemetry.addData("Coordinates (XC, YC)", String.format("(%.2f, %.2f)", XC, YC));
                     telemetry.addData("DPC",String.format("(%.2f, %.2f)", (xf+xc)-0.127,(yf+yc)-0.1778));*//*
+*/
+/*
 
                     telemetry.addData("Raw Pose x", detection.rawPose.x);
                     telemetry.addData("Raw Pose y", detection.rawPose.y);
                     telemetry.addData("Raw Pose z", detection.rawPose.z);
                     telemetry.addData("exposure", exposure.isExposureSupported());
-              */
+              *//*
+
+*/
 /*      telemetry.addData("(Xf,Yf)", Xf + ", " + Yf);*//*
+*/
+/*
 
                     telemetry.update();
                 }
@@ -166,7 +198,8 @@ public class AprilTagCustomDetection extends LinearOpMode {
     }
 }
 
-*/
+*//*
+
 
 //commented code above is without the imu yaw.
 package org.firstinspires.ftc.teamcode;
@@ -277,8 +310,10 @@ public class AprilTagCustomDetection extends LinearOpMode {
                     double xt = r * (Math.cos(angleoffset + Math.PI)); //or 180
                     double yt = r * (Math.sin(angleoffset + Math.PI));
                     //offset so that you measure from robot center
-                   /* double Xcenteroffset = 0.120645;
-                    double Ycenteroffset = 0.168275;*/
+                   */
+/* double Xcenteroffset = 0.120645;
+                    double Ycenteroffset = 0.168275;*//*
+
                     //field coordinates with the offset and added to the vector position of the april tag.
 
 
@@ -286,13 +321,158 @@ public class AprilTagCustomDetection extends LinearOpMode {
                     double FieldY = (yt + 0);
 
 
+
                     double tagyaw = 0;
-                    if (detection.id == 7) {
-                        tagyaw = Math.toDegrees(Math.PI);
+                    if (detection.id == 1) {
+                        tagyaw = Math.toDegrees(0);
 
                         double robotyawcalculated = (tagyaw + 180) - pose.yaw;
                         System.out.println(robotyawcalculated);
                     }
+                    double robotyawcalculated = (tagyaw + 180) - pose.yaw;
+
+                    telemetry.addData("robotyawcalculated", robotyawcalculated);
+                    telemetry.addData("pose", String.format("(%.2f, %.2f)", FieldX, FieldY));
+                    telemetry.addData("CorrectX", CorrectX);
+                    telemetry.addData("CorrectY", CorrectY);
+                    telemetry.addData("radius", r);
+                    telemetry.addData("angleoffset", angleoffset);
+                    telemetry.addData("xt", xt);
+                    telemetry.addData("yt", yt);
+                    telemetry.addData("Tag ID", detection.id);
+                    telemetry.addData("tagsize", detection.metadata.tagsize);
+                    telemetry.addData("x", CorrectX);
+                    telemetry.addData("y", CorrectY);
+                    telemetry.addData("z", 5);
+                    telemetry.addData("robotyaw", imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES));
+                    telemetry.addData("pose.yaw", pose.yaw);
+                    telemetry.addData("roll", pose.roll);
+                    telemetry.addData("pitch", pose.pitch);
+                    telemetry.addData("yaw", Math.toDegrees(Math.PI) - (pose.yaw));
+                    telemetry.addData("bearing (horizontal angle)", pose.bearing);
+                    telemetry.addData("elevation (vertical angle)", pose.elevation);
+                    telemetry.addData("Raw Pose x", detection.rawPose.x);
+                    telemetry.addData("Raw Pose y", detection.rawPose.y);
+                    telemetry.addData("Raw Pose z", detection.rawPose.z);
+                    telemetry.addData("exposure", exposure.isExposureSupported());
+                    telemetry.update();
+                }
+            }
+        }
+    }
+}*/
+package org.firstinspires.ftc.teamcode;
+// imports
+import android.util.Size;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.ExposureControl;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.GainControl;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagLibrary;
+import org.firstinspires.ftc.vision.apriltag.AprilTagPoseFtc;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import java.util.concurrent.TimeUnit;
+import java.util.Map;
+
+@TeleOp
+public class AprilTagCustomDetection extends LinearOpMode {
+
+    public void runOpMode() throws InterruptedException {
+        // AprilTagProcessor setup
+        AprilTagProcessor tagProcessor = new AprilTagProcessor.Builder()
+                .setDrawAxes(true)
+                .setDrawCubeProjection(true)
+                .setDrawTagID(true)
+                .setDrawTagOutline(true)
+                .setLensIntrinsics(626.731, 626.731, 642.398, 380.131)
+                .setTagFamily(AprilTagProcessor.TagFamily.TAG_36h11)
+                .setTagLibrary(AprilTagCustomLibrary.getSmallLibrary())
+                .setOutputUnits(DistanceUnit.METER, AngleUnit.RADIANS)
+                .build();
+
+        // VisionPortal setup
+        VisionPortal visionPortal = new VisionPortal.Builder()
+                .addProcessor(tagProcessor)
+                .setCamera(hardwareMap.get(WebcamName.class, "Webcam1"))
+                .setCameraResolution(new Size(1280, 720))
+                .enableLiveView(true)
+                .setStreamFormat(VisionPortal.StreamFormat.YUY2)
+                .build();
+
+        while (visionPortal.getCameraState() != VisionPortal.CameraState.STREAMING) {
+        }
+
+        // Exposure and gain control
+        ExposureControl exposure = visionPortal.getCameraControl(ExposureControl.class);
+        exposure.setMode(ExposureControl.Mode.Manual);
+        exposure.setExposure(10, TimeUnit.MILLISECONDS);
+
+        GainControl gain = visionPortal.getCameraControl(GainControl.class);
+        gain.setGain(200);
+
+        // IMU setup
+        IMU imu = hardwareMap.get(IMU.class, "imu");
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
+        imu.initialize(parameters);
+
+        // IMU yaw reset with controller and initialization
+        imu.resetYaw();
+
+        waitForStart();
+
+        while (!isStopRequested() && opModeIsActive()) {
+            if (gamepad1.x) {
+                imu.resetYaw();
+            }
+
+            tagProcessor.setPoseSolver(AprilTagProcessor.PoseSolver.APRILTAG_BUILTIN);
+
+            for (AprilTagDetection detection : tagProcessor.getDetections()) {
+                if (detection.rawPose != null) {
+                    double x = detection.rawPose.x;
+                    double y = detection.rawPose.z;
+                    double z = -detection.rawPose.y;
+
+                    Orientation rot = Orientation.getOrientation(detection.rawPose.R, AxesReference.INTRINSIC, AxesOrder.YXZ, AngleUnit.DEGREES);
+                    double yaw = -rot.firstAngle;
+                    double roll = rot.thirdAngle;
+                    double pitch = rot.secondAngle;
+                    double range = Math.sqrt(5 * 5 + y * y);
+                    double bearing = Math.toDegrees(Math.atan2(x, y));
+                    double elevation = Math.toDegrees(Math.atan2(z, Math.sqrt(x * x + y * y)));
+
+                    AprilTagPoseFtc pose = new AprilTagPoseFtc(x, y, z, yaw, roll, pitch, range, bearing, elevation);
+
+                    double CorrectX = Constants.yCorrected(pose.y);
+                    double CorrectY = -Constants.xCorrected(pose.x);
+                    double r = Math.sqrt((CorrectX * CorrectX) + (CorrectY * CorrectY));
+                    double theta = (Math.atan2(CorrectY, CorrectX));
+
+                    double robotYaw = (imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS));
+                    double angleoffset = (theta + robotYaw);
+
+                    double xt = r * (Math.cos(angleoffset + Math.PI));
+                    double yt = r * (Math.sin(angleoffset + Math.PI));
+
+                    // Retrieve AprilTagData from hashmap
+                    Constants.AprilTagData tagData = Constants.aprilTagMap.get(detection.id);
+                    double FieldX = xt + (tagData != null ? tagData.positionX : 0);
+                    double FieldY = yt + (tagData != null ? tagData.positionY : 0);
+
+                    // Retrieve tagAngle from hashmap
+                    double tagyaw = tagData != null ? tagData.tagangle : 0;
                     double robotyawcalculated = (tagyaw + 180) - pose.yaw;
 
                     telemetry.addData("robotyawcalculated", robotyawcalculated);
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -5,8 +5,6 @@ import java.util.Map;
 
 public class Constants {
     public static double yCorrected(double AY) {
-        /*  return range - (-0.106 + 0.508*(range) + -0.431*Math.pow(range,2)  + 0.223*Math.pow(range, 3) + -0.0379*Math.pow(range,4));*/
-        /* return AY - (1.13 + (-2.59*AY) + (2.43*Math.pow(AY,2)) + (-0.904*Math.pow(AY,3)) + (0.12*Math.pow(AY,4)));*/
         return AY - ((0.172 * AY) + 0.00307);
     }
 
@@ -19,13 +17,13 @@ public class Constants {
         public double positionX;
         public double positionY;
         public double size;
-        public double angle;
+        public double tagangle;
 
-        public AprilTagData(double positionX, double positionY, double size, double angle) {
+        public AprilTagData(double positionX, double positionY, double size, double tagangle) {
             this.positionX = positionX;
             this.positionY = positionY;
             this.size = size;
-            this.angle = angle;
+            this.tagangle = tagangle; // Angle already provided in radians
         }
     }
 
@@ -33,9 +31,7 @@ public class Constants {
     public static final Map<Integer, AprilTagData> aprilTagMap = new HashMap<>();
 
     static {
-
-        aprilTagMap.put(1, new AprilTagData(1.0, 2.0, 3.0, Math.toRadians(45.0)));
-        aprilTagMap.put(2, new AprilTagData(4.0, 5.0, 6.0, Math.toRadians(90.0)));
-
+        aprilTagMap.put(7, new AprilTagData(0.0, 0.0, 1.27, Math.PI / 4));
+        aprilTagMap.put(8, new AprilTagData(5.0, 5.0, 1.27, Math.PI / 2));
     }
 }
