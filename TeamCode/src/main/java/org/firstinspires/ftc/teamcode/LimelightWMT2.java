0d4dec5498aa9ddf863c42d219675aa6987509d2
Advik-highlanders28 <144296737+Advik-highlanders28@users.noreply.github.com>
2024-09-06 19:54:15 -0600

    Got progress on MegaTag2 with Limelight3g. Figured out that it won't work without limelight 3a and software support

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LimelightWMT2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LimelightWMT2.java
@@ -7,9 +7,11 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-
 import org.json.JSONArray;
 import org.json.JSONObject;
+
+@TeleOp
+
 
 
 public class LimelightWMT2 extends LinearOpMode {
